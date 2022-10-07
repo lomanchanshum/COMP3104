6ab7efdd42404389c53824eff50bb8c4402a0e8e
lomanchanshum <lokmanchan333@gmail.com>
2022-10-07 11:38:01 -0400

    adding all changes at once which includes package.json work and app src

---
+++ build/precache-manifest.a1b40f742a0a455bcf8023a5b95776e0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c2c4449b497391faa81e",
+    "url": "/lomanchanshum/COMP3104/static/css/main.e3abfa9d.chunk.css"
+  },
+  {
+    "revision": "c2c4449b497391faa81e",
+    "url": "/lomanchanshum/COMP3104/static/js/main.c2c4449b.chunk.js"
+  },
+  {
+    "revision": "d7ce395f5e653e306383",
+    "url": "/lomanchanshum/COMP3104/static/js/runtime~main.d7ce395f.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/lomanchanshum/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "54d88c067a8a6e003e433c2ed42c2b94",
+    "url": "/lomanchanshum/COMP3104/index.html"
+  }
+];
